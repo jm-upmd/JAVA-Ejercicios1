5cfcc661323cf3a2494733e1a116f05565e37dad
Jose M <jmdh@aytoalcobendas.org>
2021-11-08 16:32:14 +0100

    Actualizado

--- CJAVA-Tema  2/src/ejercicios/piramide/PiramideBis.java
+++ CJAVA-Tema  2/src/ejercicios/piramide/PiramideBis.java
@@ -1,26 +1,11 @@
-/*
-Ejemplo:
-s = número de espacios
-a = número de asteríscos.
-
-filas = 5
-
-"     *     "   s = filas; a = 1  // valores iniciales
-"    ***    "   s = s - 1; a = a + 2;
-"   *****   "   s = s - 2; a = a + 4;  
-"  *******  "   En cada iteración: 
-" ********* "       s = s-1; a = a + 2; 
-
-*/
-
 package ejercicios.piramide;
 
 import java.util.Scanner;
 
-public class Piramide {
+public class PiramideBis {
 
 	public static void main(String[] args) {
-		Scanner sss = new Scanner(System.in);
+Scanner sss = new Scanner(System.in);
 		
 		System.out.print("Introduce el número de filas: ");
 
@@ -29,7 +14,8 @@ public class Piramide {
 		int numAsteriscos = 1;
 		
 		for(int i = 1; i <= filas; i++) {        // Para cada piso de la pirámide
-			
+			numEspacioss = filas -i;
+			numAsteriscos = i*2 -1;
 			for(int j=1; j<= numEspacioss; j++) // Pinta espacios
 				System.out.print(" ");
 			
@@ -37,11 +23,10 @@ public class Piramide {
 				System.out.print("*");
 			
 			System.out.println();
-			numEspacioss--;
-			numAsteriscos+=2;
 			
 		}
 		sss.close();
+
 	}
 
 }
